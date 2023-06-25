1150225910264cd0b50503e5cd605e24e150df45
Ravi Shankar Singh <42012374+ravishankarsinghn@users.noreply.github.com>
2023-06-26 00:07:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { useState } from "react";
 const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
+    name: "Sohan",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 933372,
-    name: "Sarah",
+    name: "Mohan",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
+    name: "Ramesh",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
